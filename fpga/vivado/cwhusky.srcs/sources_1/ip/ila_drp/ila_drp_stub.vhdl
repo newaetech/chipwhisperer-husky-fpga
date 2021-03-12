4467f51ab5b4e675588f0b35ce7ef26a5bc18b90
jpcrypt <jpthibault@newae.com>
2021-03-12 10:47:46 -0500

    Add missing ILAs

---
+++ fpga/vivado/cwhusky.srcs/sources_1/ip/ila_drp/ila_drp_stub.vhdl
@@ -0,0 +1,36 @@
+-- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
+-- Date        : Tue Feb 23 15:19:22 2021
+-- Host        : qed running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/jp/GitHub/OpenTitan/husky/fpga/vivado/cwhusky.srcs/sources_1/ip/ila_drp/ila_drp_stub.vhdl
+-- Design      : ila_drp
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tftg256-2
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity ila_drp is
+  Port ( 
+    clk : in STD_LOGIC;
+    probe0 : in STD_LOGIC_VECTOR ( 6 downto 0 );
+    probe1 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    probe2 : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    probe3 : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    probe4 : in STD_LOGIC_VECTOR ( 0 to 0 );
+    probe5 : in STD_LOGIC_VECTOR ( 0 to 0 )
+  );
+
+end ila_drp;
+
+architecture stub of ila_drp is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk,probe0[6:0],probe1[0:0],probe2[15:0],probe3[15:0],probe4[0:0],probe5[0:0]";
+attribute X_CORE_INFO : string;
+attribute X_CORE_INFO of stub : architecture is "ila,Vivado 2019.2";
+begin
+end;
