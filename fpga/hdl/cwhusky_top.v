96a897c6684a13c612e6a3d1180f02219d99a3db
jpcrypt <jpthibault@newae.com>
2022-05-17 15:07:56 -0400

    Clean build (albeit with loosed ADC_DP constraint)
    Explored IDELAY on ADC_DP to help with hold violations but it makes
    timing issues pop up elsewhere in the design on observer_clock. Left if
    there around an `ifdef. Ran more testing on the bitfile, looks good.

--- fpga/hdl/cwhusky_top.v
+++ fpga/hdl/cwhusky_top.v
@@ -658,6 +658,16 @@ module cwhusky_top(
       assign ADC_data = {ADC_DP, ADC_DN};
    `else
    wire [5:0] ADC_D;
+   wire [5:0] ADC_D_delayed;
+
+   `ifdef ADC_IDELAY
+       IDELAYCTRL U_idelayctrl (
+           .RST          (reg_rst),
+           .REFCLK       (ADC_clk_fb),
+           .RDY          ()
+       );
+   `endif
+
    genvar adc_index;
    generate 
       for (adc_index = 0; adc_index < 6; adc_index = adc_index + 1) begin: gen_adc_data
@@ -671,6 +681,36 @@ module cwhusky_top(
             .IB                 (ADC_DN[adc_index])
          );
 
+         `ifdef ADC_IDELAY
+             IDELAYE2 #(
+                 .CINVCTRL_SEL              ("FALSE"), // Enable dynamic clock inversion (FALSE, TRUE)
+                 .DELAY_SRC                 ("IDATAIN"), // Delay input (IDATAIN, DATAIN)
+                 .HIGH_PERFORMANCE_MODE     ("TRUE"), // Reduced jitter ("TRUE"), Reduced power ("FALSE")
+                 .IDELAY_TYPE               ("FIXED"), // FIXED, VARIABLE, VAR_LOAD, VAR_LOAD_PIPE
+                 //.IDELAY_VALUE              (9), // Input delay tap setting (0-31); 9 x 78ps = 0.702 ns
+                 .IDELAY_VALUE              (4), // Input delay tap setting (0-31); 9 x 78ps = 0.702 ns
+                 .PIPE_SEL                  ("FALSE"), // Select pipelined mode, FALSE, TRUE
+                 .REFCLK_FREQUENCY          (200.0), // IDELAYCTRL clock input frequency in MHz (190.0-210.0, 290.0-310.0).
+                 .SIGNAL_PATTERN            ("DATA") // DATA, CLOCK input signal
+             )
+             IDELAYE2_inst (
+                 .CNTVALUEOUT               (), // 5-bit output: Counter value output
+                 .DATAOUT                   (ADC_D_delayed[adc_index]), // 1-bit output: Delayed data output
+                 .C                         (1'b0), // 1-bit input: Clock input
+                 .CE                        (1'b0), // 1-bit input: Active high enable increment/decrement input
+                 .CINVCTRL                  (1'b0), // 1-bit input: Dynamic clock inversion input
+                 .CNTVALUEIN                (5'b0), // 5-bit input: Counter value input
+                 .DATAIN                    (1'b0), // 1-bit input: Internal delay data input
+                 .IDATAIN                   (ADC_D[adc_index]), // 1-bit input: Data input from the I/O
+                 .INC                       (1'b0), // 1-bit input: Increment / Decrement tap delay input
+                 .LD                        (1'b0), // 1-bit input: Load IDELAY_VALUE input
+                 .LDPIPEEN                  (1'b0), // 1-bit input: Enable PIPELINE register to load data input
+                 .REGRST                    (1'b0) // 1-bit input: Active-high reset tap-delay input
+             );
+         `else
+             assign ADC_D_delayed = ADC_D;
+         `endif
+
          IDDR #(
             .DDR_CLK_EDGE     ("OPPOSITE_EDGE"),
             .INIT_Q1          (0),
@@ -679,7 +719,7 @@ module cwhusky_top(
          ) U_adc_iddr (
             .Q1               (ADC_data[adc_index*2]),
             .Q2               (ADC_data[adc_index*2+1]),
-            .D                (ADC_D[adc_index]),
+            .D                (ADC_D_delayed[adc_index]),
             .CE               (1'b1),
             .C                (ADC_clk_fb),
             .S                (1'b0),
