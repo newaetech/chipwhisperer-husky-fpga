0a8d8eec6e7916ad7328b4519d17648ad7d85943
jpcrypt <jpthibault@newae.com>
2023-04-24 08:50:21 -0400

    Add FIFO simulation models

--- fpga/hdl/setup.v
+++ fpga/hdl/setup.v
@@ -1,5 +1,10 @@
-`define MAX_SAMPLES 131124
-`define MAX_SEGMENT_SAMPLES 98352
+`ifdef SEMIPRO
+    `define MAX_SAMPLES 327792
+    `define MAX_SEGMENT_SAMPLES 196704
+`else
+    `define MAX_SAMPLES 131124
+    `define MAX_SEGMENT_SAMPLES 98352
+`endif
 `define HW_TYPE 10
 `define HW_VER 0
 `define SYSTEM_CLK 96000000
