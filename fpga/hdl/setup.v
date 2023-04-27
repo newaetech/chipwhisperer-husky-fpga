16a80b229ac69415701214c3044bee01e8bff817
jpcrypt <jpthibault@newae.com>
2023-04-27 14:45:30 -0400

    Change HW_TYPE for semipro

--- fpga/hdl/setup.v
+++ fpga/hdl/setup.v
@@ -1,11 +1,12 @@
 `ifdef SEMIPRO
     `define MAX_SAMPLES 327792
     `define MAX_SEGMENT_SAMPLES 196704
+    `define HW_TYPE 11
 `else
     `define MAX_SAMPLES 131124
     `define MAX_SEGMENT_SAMPLES 98352
+    `define HW_TYPE 10
 `endif
-`define HW_TYPE 10
 `define HW_VER 0
 `define SYSTEM_CLK 96000000
 `define XADC_EN
