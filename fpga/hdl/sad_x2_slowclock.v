newaetech/chipwhisperer-husky-fpga

commit e5b6b499f5e94b14e147bc167617a491ee27e117

SAD: improve timing on *slowclock implementations.

fpga/hdl/sad_x2_slowclock.v
@@ -56,7 +56,7 @@ module sad_x2_slowclock #(
     input  wire         ext_trigger,  // debug only
     input  wire         io4,  // debug only
     // verilator lint_on UNUSED
-    output reg          trigger
+    output wire         trigger
 );
 
     localparam pSADS_PER_CYCLE = 2;
@@ -67,11 +67,7 @@ module sad_x2_slowclock #(
                                        (pREF_SAMPLES <= 512)? 9 :
                                        (pREF_SAMPLES <= 1024)? 10 : 11;
 
-    reg  trigger_even;
-    reg  trigger_odd;
     reg  triggered;
-    reg  triggered_even;
-    reg  triggered_odd;
     reg [15:0] num_triggers;
     reg clear_status;
     reg clear_status_r;
@@ -88,6 +84,8 @@ module sad_x2_slowclock #(
     reg [pMASTER_COUNTER_WIDTH-1:0] master_counter_even, master_counter_odd;
     reg resetter_even [0:pREF_SAMPLES-1];
     reg resetter_odd  [0:pREF_SAMPLES-1];
+    reg trigger_even;
+    reg trigger_odd;
 
     reg individual_trigger [0:pREF_SAMPLES-1];
     reg [pSAD_COUNTER_WIDTH-1:0] sad_counter [0:pREF_SAMPLES-1];
@@ -226,28 +224,37 @@ module sad_x2_slowclock #(
    );
 
 
-
-    integer c;
-    reg trigger_r;
     always @(posedge adc_sampleclk) begin
         if (clear_status_adc || (armed_and_ready_adc && ~armed_and_ready_adc_r)) begin
             triggered <= 1'b0;
             num_triggers <= 0;
         end
-        else if (trigger && ~trigger_r) begin
+        else if (trigger) begin
             triggered <= 1'b1;
             num_triggers <= num_triggers + 1;
         end
+    end
 
-        // TODO: active check? would it be redundant?
-        trigger <= 1'b0;
-        trigger_r <= trigger;
-        for (c = 0; c < pREF_SAMPLES; c = c + 1) begin
-            if (individual_trigger[c] && ~(triggered && ~multiple_triggers)) 
-                trigger <= 1'b1;
+
+    integer c, d;
+
+    always @(posedge slow_clk_even) begin
+        trigger_even <= 1'b0;
+        for (c = 0; c < pREF_SAMPLES; c = c + pSADS_PER_CYCLE) begin
+            if (individual_trigger[c])
+                trigger_even <= 1'b1;
         end
     end
 
+    always @(posedge slow_clk_odd) begin
+        trigger_odd <= 1'b0;
+        for (d = 1; d < pREF_SAMPLES; d = d + pSADS_PER_CYCLE) begin
+            if (individual_trigger[d])
+                trigger_odd <= 1'b1;
+        end
+    end
+
+    assign trigger = trigger_even || trigger_odd;
 
     cdc_simple U_armed_and_ready_cdc (
         .reset          (reset),
@@ -375,7 +382,7 @@ module sad_x2_slowclock #(
                     sad_counter[i] <= sad_counter[i] + counter_incr_a[i] + counter_incr_b[i];
 
                 // and the triggers:
-                if ((sad_counter[i] <= threshold) && resetter_even[i] && ready2trigger_even[i])
+                if ((sad_counter[i] <= threshold) && resetter_even[i] && ready2trigger_even[i] && ~(triggered && ~multiple_triggers))
                     individual_trigger[i] <= 1'b1;
                 else
                     individual_trigger[i] <= 1'b0;
@@ -452,7 +459,7 @@ module sad_x2_slowclock #(
                     sad_counter[j] <= sad_counter[j] + counter_incr_a[j] + counter_incr_b[j];
 
                 // and the triggers:
-                if ((sad_counter[j] <= threshold) && resetter_odd[j] && ready2trigger_odd[j])
+                if ((sad_counter[j] <= threshold) && resetter_odd[j] && ready2trigger_odd[j] && ~(triggered && ~multiple_triggers))
                     individual_trigger[j] <= 1'b1;
                 else
                     individual_trigger[j] <= 1'b0;

fpga/hdl/sad_x4_slowclock.v
@@ -58,7 +58,7 @@ module sad_x4_slowclock #(
     input  wire         ext_trigger,  // debug only
     input  wire         io4,  // debug only
     // verilator lint_on UNUSED
-    output reg          trigger
+    output wire         trigger
 );
 
     localparam pSADS_PER_CYCLE = 4;
@@ -70,6 +70,10 @@ module sad_x4_slowclock #(
                                        (pREF_SAMPLES <= 1024)? 10 : 11;
 
     reg  triggered;
+    reg  trigger1;
+    reg  trigger2;
+    reg  trigger3;
+    reg  trigger4;
     reg [15:0] num_triggers;
     reg clear_status;
     reg clear_status_r;
@@ -259,26 +263,53 @@ module sad_x4_slowclock #(
    );
 
 
-    integer c;
-    reg trigger_r;
     always @(posedge adc_sampleclk) begin
         if (clear_status_adc || (armed_and_ready_adc && ~armed_and_ready_adc_r)) begin
             triggered <= 1'b0;
             num_triggers <= 0;
         end
-        else if (trigger && ~trigger_r) begin
+        else if (trigger) begin
             triggered <= 1'b1;
             num_triggers <= num_triggers + 1;
         end
+    end
 
-        // TODO: active check? would it be redundant?
-        trigger <= 1'b0;
-        trigger_r <= trigger;
-        for (c = 0; c < pREF_SAMPLES; c = c + 1) begin
-            if (individual_trigger[c] && ~(triggered && ~multiple_triggers)) 
-                trigger <= 1'b1;
+    integer c, d, e, f;
+
+    always @(posedge slow_clk1) begin
+        trigger1 <= 1'b0;
+        for (c = 0; c < pREF_SAMPLES; c = c + pSADS_PER_CYCLE) begin
+            if (individual_trigger[c])
+                trigger1 <= 1'b1;
         end
     end
+
+    always @(posedge slow_clk2) begin
+        trigger2 <= 1'b0;
+        for (d = 1; d < pREF_SAMPLES; d = d + pSADS_PER_CYCLE) begin
+            if (individual_trigger[d])
+                trigger2 <= 1'b1;
+        end
+    end
+
+    always @(posedge slow_clk3) begin
+        trigger3 <= 1'b0;
+        for (e = 2; e < pREF_SAMPLES; e = e + pSADS_PER_CYCLE) begin
+            if (individual_trigger[e])
+                trigger3 <= 1'b1;
+        end
+    end
+
+    always @(posedge slow_clk4) begin
+        trigger4 <= 1'b0;
+        for (f = 3; f < pREF_SAMPLES; f = f + pSADS_PER_CYCLE) begin
+            if (individual_trigger[f])
+                trigger4 <= 1'b1;
+        end
+    end
+
+
+    assign trigger = trigger1 || trigger2 || trigger3 || trigger4;
 
 
     cdc_simple U_armed_and_ready_cdc (
@@ -481,7 +512,7 @@ module sad_x4_slowclock #(
                     sad_counter[i] <= sad_counter[i] + counter_incr_a[i] + counter_incr_b[i] + counter_incr_c[i] + counter_incr_d[i];
 
                 // and the triggers:
-                if ((sad_counter[i] <= threshold) && resetter1[i] && ready2trigger1[i])
+                if ((sad_counter[i] <= threshold) && resetter1[i] && ready2trigger1[i] && ~(triggered && ~multiple_triggers))
                     individual_trigger[i] <= 1'b1;
                 else
                     individual_trigger[i] <= 1'b0;
@@ -577,7 +608,7 @@ module sad_x4_slowclock #(
                     sad_counter[j] <= sad_counter[j] + counter_incr_a[j] + counter_incr_b[j] + counter_incr_c[j] + counter_incr_d[j];
 
                 // and the triggers:
-                if ((sad_counter[j] <= threshold) && resetter2[j] && ready2trigger2[j])
+                if ((sad_counter[j] <= threshold) && resetter2[j] && ready2trigger2[j] && ~(triggered && ~multiple_triggers))
                     individual_trigger[j] <= 1'b1;
                 else
                     individual_trigger[j] <= 1'b0;
@@ -673,7 +704,7 @@ module sad_x4_slowclock #(
                     sad_counter[k] <= sad_counter[k] + counter_incr_a[k] + counter_incr_b[k] + counter_incr_c[k] + counter_incr_d[k];
 
                 // and the triggers:
-                if ((sad_counter[k] <= threshold) && resetter3[k] && ready2trigger3[k])
+                if ((sad_counter[k] <= threshold) && resetter3[k] && ready2trigger3[k] && ~(triggered && ~multiple_triggers))
                     individual_trigger[k] <= 1'b1;
                 else
                     individual_trigger[k] <= 1'b0;
@@ -770,7 +801,7 @@ module sad_x4_slowclock #(
                     sad_counter[l] <= sad_counter[l] + counter_incr_a[l] + counter_incr_b[l] + counter_incr_c[l] + counter_incr_d[l];
 
                 // and the triggers:
-                if ((sad_counter[l] <= threshold) && resetter4[l] && ready2trigger4[l])
+                if ((sad_counter[l] <= threshold) && resetter4[l] && ready2trigger4[l] && ~(triggered && ~multiple_triggers))
                     individual_trigger[l] <= 1'b1;
                 else
                     individual_trigger[l] <= 1'b0;

commit 3db01fdf58eddbfecdff3b2bfb9a0685afbf6e04

SAD triggering: fix issues introduced by e5b6b499f5e94b14e147bc167617a491ee27e117
1. Fix latency register.
2. Don't overcount SAD triggers.

fpga/hdl/sad_x2_slowclock.v
@@ -68,6 +68,7 @@ module sad_x2_slowclock #(
                                        (pREF_SAMPLES <= 1024)? 10 : 11;
 
     reg  triggered;
+    reg  trigger_r;
     reg [15:0] num_triggers;
     reg clear_status;
     reg clear_status_r;
@@ -149,7 +150,7 @@ module sad_x2_slowclock #(
     // used in different targets or builds.
     // Format: 2 MSB = version code (00: sad.v, 01: sad_x2_slowclock.v)
     //         6 LSB = trigger latency
-    wire [7:0] version_bits = {2'b01, 6'd12};
+    wire [7:0] version_bits = {2'b01, 6'd13};
     wire [15:0] ref_samples = pREF_SAMPLES;
 
     // register reads:
@@ -232,11 +233,12 @@ module sad_x2_slowclock #(
 
 
     always @(posedge adc_sampleclk) begin
+        trigger_r <= trigger;
         if (clear_status_adc || (armed_and_ready_adc && ~armed_and_ready_adc_r)) begin
             triggered <= 1'b0;
             num_triggers <= 0;
         end
-        else if (trigger) begin
+        else if (trigger && ~trigger_r) begin // trigger pulse will be 2 cycles of adc_sampleclk
             triggered <= 1'b1;
             num_triggers <= num_triggers + 1;
         end

fpga/hdl/sad_x4_slowclock.v
@@ -70,6 +70,7 @@ module sad_x4_slowclock #(
                                        (pREF_SAMPLES <= 1024)? 10 : 11;
 
     reg  triggered;
+    reg  trigger_r;
     reg  trigger1;
     reg  trigger2;
     reg  trigger3;
@@ -203,7 +204,7 @@ module sad_x4_slowclock #(
     // used in different targets or builds.
     // Format: 2 MSB = version code (00: sad.v, 01: sad_x2_slowclock.v, 10: sad_x4_slowclock.v)
     //         6 LSB = trigger latency
-    wire [7:0] version_bits = {2'b10, 6'd18}; // NOTE: latency not verified on-target (yet)
+    wire [7:0] version_bits = {2'b10, 6'd21}; // NOTE: latency not verified on-target (yet)
     wire [15:0] ref_samples = pREF_SAMPLES;
 
     // register reads:
@@ -270,11 +271,12 @@ module sad_x4_slowclock #(
 
 
     always @(posedge adc_sampleclk) begin
+        trigger_r <= trigger;
         if (clear_status_adc || (armed_and_ready_adc && ~armed_and_ready_adc_r)) begin
             triggered <= 1'b0;
             num_triggers <= 0;
         end
-        else if (trigger) begin
+        else if (trigger && ~trigger_r) begin // trigger pulse will be 2 cycles of adc_sampleclk
             triggered <= 1'b1;
             num_triggers <= num_triggers + 1;
         end

fpga/sim/sad_tb.v
@@ -52,7 +52,8 @@ reg clk_usb;
 reg clk_adc;
 reg reset;
 reg armed_and_ready;
-wire trigger;
+reg  trigger;
+wire trigger_presync;
 reg [11:0] adc_datain;
 
 reg setup_done;
@@ -346,19 +347,21 @@ initial begin
 
 end
 
-reg [13:0] trigger_expected_pipe;
-always @(posedge clk_adc)
-    trigger_expected_pipe <= {trigger_expected_pipe[12:0], trigger_expected};
+reg [17:0] trigger_expected_pipe;
+always @(posedge clk_adc) begin
+    trigger <= trigger_presync;
+    trigger_expected_pipe <= {trigger_expected_pipe[16:0], trigger_expected};
+end
 `ifdef SAD_X2
-    assign trigger_expected_delayed = trigger_expected_pipe[7];
+    assign trigger_expected_delayed = trigger_expected_pipe[9];
 `else
     `ifdef SAD_X4
-        assign trigger_expected_delayed = trigger_expected_pipe[13];
+        assign trigger_expected_delayed = trigger_expected_pipe[17];
     `else
         `ifdef SAD_X2B
-            assign trigger_expected_delayed = trigger_expected_pipe[6];
+            assign trigger_expected_delayed = trigger_expected_pipe[7];
         `else
-            assign trigger_expected_delayed = trigger_expected_pipe[5];
+            assign trigger_expected_delayed = trigger_expected_pipe[6];
         `endif
     `endif
 `endif
@@ -493,7 +496,7 @@ sad_wrapper #(
     .USB_WRn            (usb_wrn_out  ),
     .USB_CEn            (usb_cen_out  ),
     .USB_ALEn           (usb_alen_out ),
-    .trigger            (trigger)
+    .trigger            (trigger_presync)
 );
 
 